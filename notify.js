8ac2e7ea36cd7db541a43a1003dd57c5c8bd70c4
Felipe Orestes <FelipeOrestes@Felipes-MacBook-Pro.local>
2025-07-14 23:08:14 -0400

    Update to use secrets properly

--- notify.js
+++ notify.js
@@ -1,30 +1,39 @@
 const notifier = require('node-notifier');
 const nodemailer = require('nodemailer');
-const secrets = require('./secrets.json');
+let email, password;
+
+if (process.env.EMAIL_ADDRESS && process.env.EMAIL_APP_PASSWORD) {
+    email = process.env.EMAIL_ADDRESS;
+    password = process.env.EMAIL_APP_PASSWORD;
+} else {
+    const secrets = require('./secrets.json');
+    email = secrets.email;
+    password = secrets.appPassword;
+}
 
 function notifyDesktop(title, message) {
-  notifier.notify({
-    title,
-    message,
-    sound: true,
-  });
+    notifier.notify({
+        title,
+        message,
+        sound: true,
+    });
 }
 
 async function notifyEmail(subject, message) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: secrets.gmail.user,
-      pass: secrets.gmail.pass
-    }
-  });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: email,
+            pass: password
+        }
+    });
 
-  await transporter.sendMail({
-    from: `"Price Tracker" <${secrets.gmail.user}>`,
-    to: secrets.gmail.user,
-    subject,
-    text: message
-  });
+    await transporter.sendMail({
+        from: `"Price Tracker" <${secrets.gmail.user}>`,
+        to: secrets.gmail.user,
+        subject,
+        text: message
+    });
 }
 
 module.exports = { notifyDesktop, notifyEmail };
